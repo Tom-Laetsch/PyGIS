b4aab48eaf68b9645db3dd0c55c471826d3147eb
Tom-Laetsch <tom.laetsch@gmail.com>
2016-09-18 22:46:29 -0400

    Updated ShpFileHandler.py so that the methods were no longer static

--- ShpFileHandler.py
+++ ShpFileHandler.py
@@ -5,25 +5,27 @@ import shapefile
 class ShpWrap(object):
     def __init__(self, shpfile, names=None, verbose=False):
         self.shpfile = shpfile
+        self.names = names
         self.verbose = verbose
         self.sf = shapefile.Reader(shpfile)
         self.shapes = (self.sf).shapes()
-        self.points_dict = self.make_points_dict(self.shapes, names, self.verbose)
+        self.points_dict = self.make_points_dict()
 
-    @staticmethod
-    def make_points_dict(shapes, names=None, verbose=False):
-        num_shapes = len(shapes)
+    def make_points_dict(self):
+        names = self.names
+        num_shapes = len(self.shapes)
         if type(names) == str:
             area_prefix = names
             names = None
         else:
             area_prefix = "AREA"
-        if names == None or len(names) != num_shapes:
+        if names == None or type(names) != list or len(names) != num_shapes:
             #use default
             names = [area_prefix + "_" + str(i) for i in range(num_shapes)]
+        if self.names != names: self.names = names
         points_dict = dict()
         for i in range(num_shapes):
-            parts = shapes[i].parts
+            parts = self.shapes[i].parts
             pR = 0
             tot_xs = []
             tot_ys = []
@@ -33,14 +35,14 @@ class ShpWrap(object):
                     ys = []
                     pL = parts[j]
                     pR = parts[j+1]
-                    for x,y in shapes[i].points[pL:pR]:
+                    for x,y in self.shapes[i].points[pL:pR]:
                         xs.append(x)
                         ys.append(y)
                     tot_xs.append(xs)
                     tot_ys.append(ys)
             xs = []
             ys = []
-            for x,y in shapes[i].points[pR:]:
+            for x,y in self.shapes[i].points[pR:]:
                 xs.append(x)
                 ys.append(y)
             tot_xs.append(xs)
@@ -48,13 +50,12 @@ class ShpWrap(object):
             points_dict[names[i]] = zip(tot_xs,tot_ys)
         return points_dict
 
-    @staticmethod
-    def make_points_list(points_dict, verbose=False):
+    def make_points_list(self):
         xs = []
         ys = []
         names = []
-        for key in points_dict.keys():
-            for x,y in points_dict[key]:
+        for key in self.points_dict.keys():
+            for x,y in self.points_dict[key]:
                 names.append(key)
                 xs.append(x)
                 ys.append(y)
