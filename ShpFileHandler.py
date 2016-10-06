5871ce0e4deb779c0e69d71f59ef43d7449b9897
Thomas Laetsch <tom.laetsch@gmail.com>
2016-10-06 14:35:04 -0400

    Updated ShpFileHandler to make work easier

--- ShpFileHandler.py
+++ ShpFileHandler.py
@@ -4,17 +4,16 @@ import shapefile
 
 class ShpWrap(object):
     def __init__(self, shpfile, names=None, verbose=False):
-        self.shpfile = shpfile
+        self._shpfile = shpfile
         self.verbose = verbose
-        self.sf = shapefile.Reader(shpfile)
-        #self.shapes = (self.sf).shapes()
-        self.points_dict = self._make_points_dict()
+        self._sfreader = shapefile.Reader(shpfile)
+        self._points_dict = self._make_points_dict()
 
     def _make_points_dict(self):
         points_dict = dict()
         names = []
         name_cnt = 0
-        for record, shape in zip(self.sf.iterRecords(), self.sf.iterShapes()):
+        for record, shape in zip(self._sfreader.iterRecords(), self._sfreader.iterShapes()):
             names.append(record[4])
             parts = shape.parts
             pR = 0
@@ -42,12 +41,16 @@ class ShpWrap(object):
             name_cnt += 1
         return points_dict
 
+    @property
+    def points_dict(self):
+        return self._points_dict
+
     def make_points_list(self):
         xs = []
         ys = []
         names = []
-        for key in self.points_dict.keys():
-            for x,y in self.points_dict[key]:
+        for key in self._points_dict.keys():
+            for x,y in self._points_dict[key]:
                 names.append(key)
                 xs.append(x)
                 ys.append(y)
